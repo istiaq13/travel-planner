4c2049bec1ed2a8527d6335cfd8041e13cda3fda
Mohammad Istiaq Prodhan <istiaqprodhan02@gmail.com>
2025-05-18 13:00:59 +0600

    setup-done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Home from '@/components/Home/home';
 import React from 'react'
 
 const HomePage = () => {
-  return <div className="text-5xl">HomePage</div>
+  return (
+  <div>
+    <Home/>
+  </div>
+  );
 }
 
 export default HomePage;
